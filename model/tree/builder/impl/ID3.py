ca4543197ffdc9b2bfaef4cdabcc900fa5d3e44f
Harrison Cole <harrison.cole@mnfgroup.limited>
2021-09-11 01:21:38 +1000

    - WIP: DecisionTrees

--- model/tree/builder/impl/ID3.py
+++ model/tree/builder/impl/ID3.py
@@ -2,6 +2,40 @@ import math
 import numpy as np
 from model.tree.builder.builder import DecisionTreeBuilder
 from model.tree.node import Node
+from model.tree.pivot import Pivot
+from utilities import require
+
+
+class PivotCandidate:
+
+    def __init__(self, feature: int, gain: float, probe: float):
+        self.__feature = feature
+        self.__gain = gain
+        self.__probe = probe
+
+    # @property
+    def feature(self) -> int:
+        return require(self.__feature, 'feature')
+
+    # @property
+    def gain(self) -> float:
+        return require(self.__gain, 'gain')
+
+    # @property
+    def probe(self) -> float:
+        return require(self.__probe, 'probe')
+
+    def update(self, feature: int, gain: float, probe: float) -> bool:
+        if gain < self.gain():
+            return False
+        self.__feature = feature
+        self.__gain = gain
+        self.__probe = probe
+        return True
+
+    @staticmethod
+    def initial() -> 'PivotCandidate':
+        return PivotCandidate(0, -math.inf, 0.5)
 
 
 class ID3DecisionTreeBuilder(DecisionTreeBuilder):
@@ -11,25 +45,43 @@ class ID3DecisionTreeBuilder(DecisionTreeBuilder):
         choices = len(classes)
 
         if choices <= 0:  # no choices
-            default_value = 0  # TODO: get default value
+            default_value = '<default-value>'  # TODO: get default value
             return Node.terminate(default_value)
 
         if choices == 1:  # one clear choice
             return Node.terminate(classes[0])
 
+        candidate: PivotCandidate = PivotCandidate.initial()
         attributes = x.shape[1]
         for index in range(attributes):
+            # TODO: handle continuous and categorical attributes
             attribute = x[:, index]
             probes = ID3DecisionTreeBuilder.create_probe_values(attribute.min(), attribute.max())
             for probe in probes:
-                pass
-                # TODO: measure gain ...
-            pass
-        # maxima = -math.inf
-        # TODO: assume attributes are continuous (account for categorical later)
+                # gain = self.purity(attribute, probe, x, y)  # TODO: compute information gain
+                gain = self.measure_progress(y, attribute, probe)
+                candidate.update(feature=index, gain=gain, probe=probe)
+
+        # TODO: sanity check candidate or build pivot from candidate
+        pivot: Pivot = Pivot.build(candidate.feature(), candidate.probe())
+        # TODO: use or apply pivot data-structure and make more efficient...
+        idx_lower = x[:, candidate.feature()] <= candidate.probe()
+        idx_upper = x[:, candidate.feature()] > candidate.probe()
+
+        return Node.branch(pivot, self.build(x[idx_lower], y[idx_lower]), self.build(x[idx_upper], y[idx_upper]))
 
     def purity(self, attribute, x, y) -> float:
-        raise NotImplementedError('ID3DecisionTreeBuilder#purity')
+        return np.random.random()
+        # raise NotImplementedError('ID3DecisionTreeBuilder#purity')
+
+    def measure_progress(self, y, attribute, threshold) -> float:
+        return 0.0
+
+    @staticmethod
+    def compute_entropy(y, eps=1e-9) -> float:
+        classes, counts = np.unique(y, return_counts=True)
+        frequency = counts / len(y)
+        return -(frequency * np.log2(frequency + eps)).sum()
 
     @staticmethod
     def create_probe_values(minima, maxima):
--- model/tree/pivot.py
+++ model/tree/pivot.py
@@ -3,12 +3,12 @@ from utilities import require
 
 class Pivot:
 
-    def __init__(self, feature: str, predicate: lambda x: bool):
+    def __init__(self, feature, predicate: lambda x: bool):
         self.__feature = require(feature, 'feature')
         self.__predicate = require(predicate, 'predicate')
 
     @property
-    def feature(self) -> str:
+    def feature(self):
         return self.__feature
 
     @property
@@ -17,3 +17,11 @@ class Pivot:
 
     def split(self, value: any) -> bool:
         return self.predicate(value)
+
+    @staticmethod
+    def build(attribute, probe) -> 'Pivot':
+        feature = f'x[{attribute}] <= {probe}'
+
+        def predicate(x):
+            return x[feature] <= probe
+        return Pivot(feature=feature, predicate=predicate)
