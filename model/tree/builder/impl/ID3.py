5c25f6fb9adff84a4fe07594e1046b29a19a3a74
Harrison Cole <harrison.cole@mnfgroup.limited>
2021-09-10 16:12:47 +1000

    - WIP: DecisionTrees

--- model/tree/builder/impl/ID3.py
+++ model/tree/builder/impl/ID3.py
@@ -17,7 +17,21 @@ class ID3DecisionTreeBuilder(DecisionTreeBuilder):
         if choices == 1:  # one clear choice
             return Node.terminate(classes[0])
 
-        maxima = -math.inf
+        attributes = x.shape[1]
+        for index in range(attributes):
+            attribute = x[:, index]
+            probes = ID3DecisionTreeBuilder.create_probe_values(attribute.min(), attribute.max())
+            for probe in probes:
+                pass
+                # TODO: measure gain ...
+            pass
+        # maxima = -math.inf
+        # TODO: assume attributes are continuous (account for categorical later)
 
     def purity(self, attribute, x, y) -> float:
         raise NotImplementedError('ID3DecisionTreeBuilder#purity')
+
+    @staticmethod
+    def create_probe_values(minima, maxima):
+        return [v * minima + (1.0 - v) * maxima for v in [0.75, 0.5, 0.25]]  # TODO: expand values
+
